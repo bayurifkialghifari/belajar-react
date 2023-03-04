2715b7b23c859077cb04b8708db17ffa8e85d747
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2023-03-04 21:04:59 +0700

    add sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,72 @@
 import React from 'react'
+import {
+  NavLink, Link
+} from 'react-router-dom'
+import { RiHomeFill } from 'react-icons/ri'
+import { IoArrowForward } from 'react-icons/io5'
+import logo from '../assets/logo.png'
 
-const Sidebar = () => {
+// Css
+const IsActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize'
+const IsNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize'
+
+// List categories
+const categories = [
+  { name: 'Binatang' },
+  { name: 'Gaming' },
+  { name: 'Humor' },
+  { name: 'Hiburan' },
+  { name: 'Lainnya' },
+]
+
+const Sidebar = ({ user, closeToggle }) => {
+  const handleColseSidebar = () => {
+    if(closeToggle) closeToggle(false)
+  }
+
   return (
-    <div>Sidebar</div>
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scrikk min-w-210 hide-scrollbar">
+      <div className="flex flex-col">
+        <Link
+          to="/"
+          className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+          onClick={handleColseSidebar}
+        >
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+        <div className="flex flex-col gap-5">
+          <NavLink
+            to="/ggwp"
+            className={({ isActive }) => isActive ? IsActiveStyle : IsNotActiveStyle}
+            onClick={handleColseSidebar}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">Discover categories</h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`category/${category.name}`}
+              className={({ isActive }) => isActive ? IsActiveStyle : IsNotActiveStyle}
+              onClick={handleColseSidebar}
+              key={category.name}
+            >
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleColseSidebar}
+        >
+          <img src={user.image} className="w-10 h-10 rounded-full" />
+          <p>{user.username}</p>
+        </Link>
+      )}
+    </div>
   )
 }
 
